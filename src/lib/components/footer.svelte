874d2dc3440873fe8d0d24733e3c7155501e0b98
Rahman Yilmaz <85690773+Sudo-Rahman@users.noreply.github.com>
2024-07-14 09:47:21 +0300

    setup github action to CI/CD

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+    <div class="w-full p-7 bg-accent text-center flex justify-center">
+        <p>&copy; {currentYear} Sudo-Rahman. All rights reserved.</p>
+    </div>
+</footer>
+
+<style>
+</style>
